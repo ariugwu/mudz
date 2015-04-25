33397c9ca773aa8f2440eb075d797ee35709bd76
Ari Ugwu <ari@drybydesign.com>
2015-04-25 10:03:23 -0400

    Forgot the room content file!

---
+++ mudz.Core/Model/Domain/Environment/Map/Room/RoomContent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using mudz.Core.Model.Domain.Inventory;
+using mudz.Core.Model.Domain.Monster;
+using mudz.Core.Model.Domain.Npc;
+using mudz.Core.Model.Domain.Player;
+
+namespace mudz.Core.Model.Domain.Environment.Map.Room
+{
+    public class RoomContent
+    {
+        public RoomContent(RoomKey roomKey)
+        {
+            RoomKey = roomKey;
+        }
+
+        public RoomKey RoomKey { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<IGameObject> GameObjects { get; set; }
+    }
+}
