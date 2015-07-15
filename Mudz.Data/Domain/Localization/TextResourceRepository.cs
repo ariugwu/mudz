55043b7e69f514bf1854e922859eb3c397a78f98
Tommy Long <tommy.long@4com.co.uk>
2015-07-15 07:50:36 +0100

    Switched enumerable to yield

--- Mudz.Data/Domain/Localization/TextResourceRepository.cs
+++ Mudz.Data/Domain/Localization/TextResourceRepository.cs
@@ -9,29 +9,26 @@ namespace Mudz.Data.Domain.Localization
     {
         public static IEnumerable<TextResource> GetResources()
         {
-            var resources = new List<TextResource>();
 
             //* CannotRespondRoom
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.CannotRespondRoomMessage, Culture = "en-us", Content = "{0} begins to move and then collapses!" });
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.CannotRespondPlayerMessage, Culture = "en-us", Content = "You try to move and collapse. Perhaps you need rest?" });
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.CannotRespondTargetMessage, Culture = "en-us", Content = "" });
+            yield return new TextResource() { TextResourceName = TextResourceNames.CannotRespondRoomMessage, Culture = "en-us", Content = "{0} begins to move and then collapses!" };
+            yield return new TextResource() { TextResourceName = TextResourceNames.CannotRespondPlayerMessage, Culture = "en-us", Content = "You try to move and collapse. Perhaps you need rest?" };
+            yield return new TextResource() { TextResourceName = TextResourceNames.CannotRespondTargetMessage, Culture = "en-us", Content = "" };
 
             //* NotEnoughStamina
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.NotEnoughStaminaRoomMessage, Culture = "en-us", Content = "{0} does not have enough stamina (turns) to complete this action!" });
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.NotEnoughStaminaPlayerMessage, Culture = "en-us", Content = "You do not have enough stamia (turns) to complete this action!" });
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.NotEnoughStaminaTargetMessage, Culture = "en-us", Content = "" });
+            yield return new TextResource() { TextResourceName = TextResourceNames.NotEnoughStaminaRoomMessage, Culture = "en-us", Content = "{0} does not have enough stamina (turns) to complete this action!" };
+            yield return new TextResource() { TextResourceName = TextResourceNames.NotEnoughStaminaPlayerMessage, Culture = "en-us", Content = "You do not have enough stamia (turns) to complete this action!" };
+            yield return new TextResource() { TextResourceName = TextResourceNames.NotEnoughStaminaTargetMessage, Culture = "en-us", Content = "" };
 
             //* Fight
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.FightRoomMessage, Culture = "en-us", Content = "" });
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.FightPlayerMessage, Culture = "en-us", Content = "" });
-            resources.Add(new TextResource() { TextResourceName = TextResourceNames.FightTargetMessage, Culture = "en-us", Content = "" });
+            yield return new TextResource() { TextResourceName = TextResourceNames.FightRoomMessage, Culture = "en-us", Content = "" };
+            yield return new TextResource() { TextResourceName = TextResourceNames.FightPlayerMessage, Culture = "en-us", Content = "" };
+            yield return new TextResource() { TextResourceName = TextResourceNames.FightTargetMessage, Culture = "en-us", Content = "" };
 
             ////* 
-            //resources.Add(new TextResource() { TextResourceName = TextResourceNames., Culture = "en-us", Content = "" });
-            //resources.Add(new TextResource() { TextResourceName = TextResourceNames., Culture = "en-us", Content = "" });
-            //resources.Add(new TextResource() { TextResourceName = TextResourceNames., Culture = "en-us", Content = "" });
-
-            return resources;
+            //yield return new TextResource() { TextResourceName = TextResourceNames., Culture = "en-us", Content = "" };
+            //yield return new TextResource() { TextResourceName = TextResourceNames., Culture = "en-us", Content = "" };
+            //yield return new TextResource() { TextResourceName = TextResourceNames., Culture = "en-us", Content = "" };
         }
 
         public static Dictionary<TextResourceNames, string> TextResourceLookUpByCulture(string culture)
