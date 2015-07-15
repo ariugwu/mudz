cdc1ab54f3ff5597cbd30996b5f3912e2dabdb59
Tommy Long <tommy.long@4com.co.uk>
2015-07-15 07:46:03 +0100

    Improved room equality hashcoding

--- Mudz.Common/Domain/Environment/Map/Room/RoomKeyEqualityComparer.cs
+++ Mudz.Common/Domain/Environment/Map/Room/RoomKeyEqualityComparer.cs
@@ -11,7 +11,13 @@ namespace Mudz.Common.Domain.Environment.Map.Room
 
         public int GetHashCode(RoomKey obj)
         {
-            return obj.X.GetHashCode()*obj.Y;
+			unchecked
+			{
+				int hash = 17;
+				hash = hash * 23 + obj.X.GetHashCode();
+				hash = hash * 23 + obj.Y.GetHashCode();
+				return hash;
+			}
         }
     }
 }
